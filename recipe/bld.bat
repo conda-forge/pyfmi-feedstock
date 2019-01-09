0dedc243f5e58675d30c568f4a2580d83183fee5
Julien Schueller <schueller@phimeca.com>
2019-01-09 09:49:22 +0100

    del src\pyfmi\*.c

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,5 @@
 
-%PYTHON% setup.py build_ext --inplace --fmil-home="%LIBRARY_PREFIX%"
-if errorlevel 1 exit 1
+del src\pyfmi\*.c
 
 %PYTHON% setup.py install --fmil-home="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
