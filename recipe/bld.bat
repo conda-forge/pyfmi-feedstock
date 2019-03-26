f1ac254426ba716256518004b414cff8ddb7a70e
Julien Schueller <schueller@phimeca.com>
2019-03-26 12:02:39 +0100

    Use Github tarball

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,3 @@
 
-del src\pyfmi\*.c
-
 %PYTHON% setup.py install --fmil-home="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-rm src/pyfmi/*.c
-
 ${PYTHON} setup.py install --fmil-home=${PREFIX}
