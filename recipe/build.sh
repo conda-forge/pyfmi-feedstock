38a98e24eef9612948b0d0098cc55e01669b983e
Julien Schueller <schueller@phimeca.com>
2024-05-17 22:42:00 +0200

    distutils

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+git clone --depth 1 https://github.com/modelon-community/PyFMI.git
+cd PyFMI
+rm setup.cfg
+patch -p1 -i ${RECIPE_DIR}/py312.patch
+
 ${PYTHON} setup.py install --fmil-home=${PREFIX}
