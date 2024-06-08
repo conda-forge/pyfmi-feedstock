1e6d2b607460d284683ce54e8db2fff8feeb9d30
Julien Schueller <schueller@phimeca.com>
2024-06-08 09:44:11 +0200

    Ignore dependencies

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
 
+del setup.cfg
+
 %PYTHON% setup.py install --fmil-home="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# iggnore dependencies
+rm setup.cfg
+
 ${PYTHON} setup.py install --fmil-home=${PREFIX}
