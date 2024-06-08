b402a4e42d1ac179ed4ee54290dd22dccfa21bd7
Julien Schueller <schueller@phimeca.com>
2024-06-08 09:59:18 +0200

    bdist_wheel

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,7 @@
 # iggnore dependencies
 rm setup.cfg
 
-${PYTHON} setup.py install --fmil-home=${PREFIX}
+# python setup.py install fails on osx so build wheel first
+${PYTHON} setup.py bdist_wheel --fmil-home=${PREFIX}i
+find . -name "*.whl"
+${PYTHON} -m pip install dist/*.whl --no-deps -vv
