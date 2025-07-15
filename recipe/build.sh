fd87da35b54854db49fbfb4511895c658768e8d0
Julien Schueller <schueller@phimeca.com>
2025-07-15 16:30:15 +0200

    Typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# iggnore dependencies
+# ignore dependencies
 rm setup.cfg
 
 # python setup.py install fails on osx so build wheel first
-${PYTHON} setup.py bdist_wheel --fmil-home=${PREFIX}i
+${PYTHON} setup.py bdist_wheel --fmil-home=${PREFIX}
 find . -name "*.whl"
 ${PYTHON} -m pip install dist/*.whl --no-deps -vv
