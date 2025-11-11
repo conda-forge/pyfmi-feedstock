18ef76d48531aa8d6a74c50029b55adc289ef1df
Julien Schueller <schueller@phimeca.com>
2025-11-11 19:35:30 +0100

    win: static link

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 
 del setup.cfg
 
-%PYTHON% setup.py install --fmil-home="%LIBRARY_PREFIX%"
+%PYTHON% setup.py install --fmil-home="%LIBRARY_PREFIX%" --fmil-name=fmilib --extra-c-flags="-DFMILIB_STATIC_LIB_ONLY"
 if errorlevel 1 exit 1
